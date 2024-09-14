6ca3b99d5f36ff5887bba46c69b7a8e4dc721aa7
krithi-ks <141476262+krithi-ks@users.noreply.github.com>
2024-09-14 17:55:22 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,34 @@
+// Switch-case
+/* switch(condition)
+   {
+      case 1:
+         // Statement;
+         break;
+       .
+       .
+      case n:
+         // Statement;
+         break;
+    
+      default:
+         // Statement;
+   }
+*/
+
+// Loops
+/* while
+   do-while
+   for
+*/
+
+// Arrays
+
+// Object 
+/* let <object_name> = {
+       <property_name>: value,
+       <property_name2>: value2
+}
+
+To access object,
+     (object_name.property_name)
+*/
