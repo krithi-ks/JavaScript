6969c962f9bf7a277d2ca16d1922809995f5a478
krithi-ks <141476262+krithi-ks@users.noreply.github.com>
2024-09-14 12:30:20 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,32 @@
+// Operators
+
+/* Operation
+   Operands
+   Operator
+*/
+
+/* Unary Operator - 1 operand
+   Binary Operator - 2 operands
+   Ternary Operator - 3 operands
+*/
+
+// Binary
+/* Arithmetic operators + - * / %
+   Relational operators < > <= >= == != (T/F)
+   Assignment operators = += -= *= /=
+   Logical operators && || !
+   Identity operator === !==
+*/
+
+// Unary ++ -- 
+/* Increment - Post increment a++, Pre increment ++a
+   Decrement - Post decrement a--, Pre decrement --a
+*/
+
+
+// Conditional Statements
+/* if
+   if else
+   if else-if else
+   switch
+*/
